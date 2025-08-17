a77cca06af4cc2a9ccd5fae5ea7dab14e9a1429d
Ben King <ben@aviato.consulting>
2025-08-17 21:46:15 +1000

    cr to cr jobs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,4 @@
 
-output "cloud_run_service_url" {
-  description = "The URL of the Cloud Run service."
-  value       = google_cloud_run_v2_service.main.uri
-}
-
 output "sql_instance_connection_name" {
   description = "The connection name of the SQL instance."
   value       = google_sql_database_instance.main.connection_name
